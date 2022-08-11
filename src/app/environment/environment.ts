c460a05e6984257c7b20bcfd1876cf2ed56cf221
smswetha <swethasm17022001@gmail.com>
2022-08-11 08:04:58 +0530

    5 files modified

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -2,3 +2,8 @@ export const environment = {
   production: false,
   dummyurl: 'https://dummyjson.com/',
 };
+
+
+// fetch('https://dummyjson.com/products/1')
+// .then(res => res.json())
+// .then(console.log);
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -18,3 +18,4 @@ export class ProductComponent implements OnInit {
     });
   }
 }
+
